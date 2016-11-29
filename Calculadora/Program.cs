6d488372fc79b4079b10c2afa674618e79b953d4
SampaioJunior <juniorsam1@hotmail.com>
2016-11-29 15:56:30 -0200

    Começando com C-Sharp

---
+++ Calculadora/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MiniCalculadora
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+         double numero1, numero2;
+         string temporario;
+         double resultado;
+
+         Console.WriteLine("Digite um numero");
+         temporario = Console.ReadLine();
+         numero1 = int.Parse(temporario);
+        
+         Console.WriteLine("Digite mais um numero");
+         temporario = Console.ReadLine();
+         numero2 = int.Parse(temporario);
+
+         resultado = numero1 + numero2;
+         Console.WriteLine("Soma: " + resultado);
+
+         resultado = numero1 - numero2;
+         Console.WriteLine("Subratacao: " + resultado);
+         
+         resultado = numero1 * numero2;
+         Console.WriteLine("Multiplicacao: " + resultado);
+
+         resultado = numero1 / numero2;
+         Console.WriteLine("divisao: " + resultado);
+
+        }
+    }
+}
---
+++ Conversão de dados/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConvertendoStrToInt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "10000";
+            int i;
+            
+            i = int.Parse(s);
+
+            int x = i + 10;
+            Console.WriteLine(x);
+        }
+    }
+}
